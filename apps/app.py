f18622a47718a72e0feb18b20d0a171279ba3159
matsudatatsuya <matudatatuya7@gmail.com>
2023-07-02 14:45:05 +0900

    signup機能を追加した

--- apps/app.py
+++ apps/app.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from flask import Flask
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
@@ -7,6 +8,10 @@ from apps.config import config
 
 db = SQLAlchemy()
 csrf = CSRFProtect()
+
+login_manager = LoginManager()
+login_manager.login_view = "auth.signup"
+login_manager.login_message = ""
 
 def create_app(config_key):
     app = Flask(__name__)
@@ -16,8 +21,9 @@ def create_app(config_key):
     csrf.init_app(app)
     db.init_app(app)
     Migrate(app, db)
-    
 
+    login_manager.init_app(app)
+    
     from apps.crud import views as crud_views
 
     app.register_blueprint(crud_views.crud, url_prefix="/crud")

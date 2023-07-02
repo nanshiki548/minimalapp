b6316ea6f918b895a016f18c17d3ead415bb6a45
matsudatatsuya <matudatatuya7@gmail.com>
2023-07-02 15:06:38 +0900

    ログアウト機能を実装

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -2,7 +2,7 @@ from apps.app import db
 from apps.auth.forms import SignUpForm, LoginForm
 from apps.crud.models import User
 from flask import Blueprint, render_template, render_template, flash, url_for, redirect, request
-from flask_login import login_user
+from flask_login import login_user, logout_user
 
 auth = Blueprint(
     "auth",
@@ -52,6 +52,11 @@ def login():
         if user is not None and user.verify_password(form.password.data):
             login_user(user)
             return redirect(url_for("crud.users"))
-        #ログイン失敗メッセージ
+        # ログイン失敗メッセージ
         flash("メールアドレスかパスワードが不正です")
     return render_template("auth/login.html", form=form)
+
+@auth.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("auth.login"))

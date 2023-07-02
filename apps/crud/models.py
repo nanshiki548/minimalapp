ca9d36860ff86a58985c37f4f2c7ce434ab0b347
matsudatatsuya <matudatatuya7@gmail.com>
2023-07-02 12:05:05 +0900

    crud機能をつけた

---
+++ apps/crud/models.py
@@ -0,0 +1,24 @@
+from datetime  import datetime
+
+from apps.app import db
+from werkzeug.security import generate_password_hash
+
+# db.Modelを継承したUserクラスを作成する
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, index=True)
+    email = db.Column(db.String, unique=True, index=True)
+    password_hash = db.Column(db.String)
+    creat_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+    # パスワードをセットするためのプロパティ
+    @property
+    def password(self):
+        raise AttributeError("読み取り不可")
+    
+    # パスワードをセットするためのセッター関数でハッシュ化したパスワードをリセットする
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
